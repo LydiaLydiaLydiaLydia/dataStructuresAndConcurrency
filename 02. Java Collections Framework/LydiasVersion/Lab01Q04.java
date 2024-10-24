f9ffbb6abd3437ef4972a718952fa76ac92b0be3
t00222338 <lydia.sheehan2@students.ittralee.ie>
2024-10-24 14:58:45 +0100

    #Lab 1 Java Collections Framework finished

--- 02. Java Collections Framework/LydiasVersion/Lab01Q04.java
+++ 02. Java Collections Framework/LydiasVersion/Lab01Q04.java
@@ -1,17 +1,19 @@
 package LydiasVersion;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.List;
 
 public class Lab01Q04 {
     public static void main(String[] args) {
         String fileName = "C:/Users/t00222338/Desktop/hamlet.txt";
-        TreeSet<String> hamletTreeSet = new TreeSet<>();
+        Map<String, Integer> hamMap = new HashMap<String, Integer>();
+        int lineNumber = 1;
         try {
             Scanner in = new Scanner(new File(fileName));
             while (in.hasNextLine()) {
@@ -20,7 +22,8 @@ public class Lab01Q04 {
                 lineParser.useDelimiter("[^A-Za-z0-9]+");
                 while (lineParser.hasNext()) {
                     String word = lineParser.next();
-                    hamletTreeSet.add(word);
+                    hamMap.put(word, lineNumber);
+                    lineNumber++;
                 }
                 lineParser.close();
             }
@@ -29,18 +32,79 @@ public class Lab01Q04 {
             e.printStackTrace();
         }
 
-        try{
-            FileWriter writer = new FileWriter(fileName, true);
-            writer.write("Each word from the tree set: \n");
-            Iterator<String> iterator = hamletTreeSet.iterator();
-            while (iterator.hasNext()) {
-                String word = iterator.next();
-                writer.write(word + " ");
+        System.out.println("The set of words along with their most recent line appearance: ");
+        Set<String> keySet = hamMap.keySet();
+        for (String key : keySet) {
+            System.out.println(key + " : " + hamMap.get(key));
+        }
+
+        //using an ArrayList to store the line numbers
+        Map<String, List> hamletMapArray = new HashMap<>();
+        lineNumber = 1;
+        try {
+            Scanner in = new Scanner(new File(fileName));
+            while (in.hasNextLine()) {
+                Scanner lineParser = new Scanner(in.nextLine());
+                // Use any characters other than a-z, A-Z, 0-9 as delimiters
+                lineParser.useDelimiter("[^A-Za-z0-9]+");
+                while (lineParser.hasNext()) {
+                    String word = lineParser.next();
+                    if (hamletMapArray.containsKey(word)) {
+                        hamletMapArray.get(word).add(lineNumber);
+                    }else{
+                        List<Integer> lineList = new ArrayList<>();
+                        lineList.add(lineNumber);
+                        hamletMapArray.put(word, lineList);
+                    }
+                    lineNumber++;
+                }
+                lineParser.close();
             }
-            writer.write("\nThe size of the tree set is " + hamletTreeSet.size() + "\n");
-            writer.close();
-        } catch (IOException e) {
+            in.close();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        System.out.println("\nThe words in the excerpt and the lines they occurred in (ArrayList version)");
+        System.out.println(hamletMapArray);
+
+        //using a set to store the line numbers
+        Map<String, Set> hamletMapSet = new HashMap<>();
+        lineNumber = 1;
+        try {
+            Scanner in = new Scanner(new File(fileName));
+            while (in.hasNextLine()) {
+                Scanner lineParser = new Scanner(in.nextLine());
+                // Use any characters other than a-z, A-Z, 0-9 as delimiters
+                lineParser.useDelimiter("[^A-Za-z0-9]+");
+                while (lineParser.hasNext()) {
+                    String word = lineParser.next();
+                    if (hamletMapSet.containsKey(word)) {
+                        hamletMapSet.get(word).add(lineNumber);
+                    }else{
+                        Set<Integer> lineList = new HashSet<>();
+                        lineList.add(lineNumber);
+                        hamletMapSet.put(word, lineList);
+                    }
+                    lineNumber++;
+                }
+                lineParser.close();
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\nThe words in the excerpt and the lines they occurred in (HashSet version)");
+        System.out.println(hamletMapSet);
+
+        //performing a search on the Map for a key value
+        //looking for the word 'to'
+        if (hamletMapSet.containsKey("to")) {
+            System.out.println("\nThe map contains the word you were looking for, the word appears at line(s):");
+            System.out.println(hamletMapArray.get("to"));
+        }
+        System.out.println("\nUsing .get() to search for a word that isn't there: ");
+        System.out.println(hamletMapArray.get("aubergine"));
+
+
     }
 }
