c5e327a6e87717e531c0f245bfaaec5f471ade23
Lydia Sheehan <Lydia.Sheehan2@students.ittralee.ie>
2024-11-08 14:57:02 +0000

    #Lab 4 Working on recursive Fibonacci

--- 06. Recursion/RecursionLydiasVersion/Q4Fibonacci.java
+++ 06. Recursion/RecursionLydiasVersion/Q4Fibonacci.java
@@ -3,21 +3,24 @@ package RecursionLydiasVersion;
 public class Q4Fibonacci {
 
     public static int FibonacciRecursive(int n) {
-        return 0;
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        else{
+            return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+        }
     }
     public static int FibonacciNonRecursive(int n) {
         if(n == 0) return 0;
         else if (n == 1) return 1;
         else{
             int last = 1;
-            int secondLast = 1;
-            int temp;
+            int secondLast = 0;
+            int current;
             for(int i = 2; i <= n; i++){
                 //add last to second last
-                temp = last + secondLast;
+                current = last + secondLast;
                 secondLast = last;
-                last = temp;
-                i++;
+                last = current;
             }
             return last;
         }
---
+++ 06. Recursion/RecursionLydiasVersion/package1/FibonacciDriver.java
@@ -0,0 +1,10 @@
+package RecursionLydiasVersion.package1;
+
+import static RecursionLydiasVersion.Q4Fibonacci.FibonacciRecursive;
+
+public class FibonacciDriver {
+    public static void main(String[] args) {
+        FibonacciRecursive(2);
+        FibonacciRecursive(8);
+    }
+}
